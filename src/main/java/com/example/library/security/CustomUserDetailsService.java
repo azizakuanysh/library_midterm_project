bd96c016738d1950bd7d20ed0d00a66fd847c7a7
kuanyshh.uralsk@gmail.com <kuanyshh.uralsk@gmail.com  git config --global user.name azizakuanysh>
2026-05-22 03:59:16 +0500

    CustomUserDetailsService tailored for DTO

--- src/main/java/com/example/library/security/CustomUserDetailsService.java
+++ src/main/java/com/example/library/security/CustomUserDetailsService.java
@@ -6,12 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService
-        implements UserDetailsService {
+        implements UserDetailsService, PasswordEncoder {
 
     private final UserRepository userRepository;
 
@@ -29,5 +30,15 @@ public class CustomUserDetailsService
                 .password(user.getPassword())
                 .roles(user.getRole().name())
                 .build();
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return "";
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return false;
     }
 }
